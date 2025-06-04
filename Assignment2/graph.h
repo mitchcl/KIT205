2259f6ac5c63c0f6e8c4ff7ea3b78f817c3b6411
Mitch <mitchclifford123@hotmail.com>
2025-06-04 13:43:00 +1000

    essential services

--- Assignment2/graph.h
+++ Assignment2/graph.h
@@ -14,6 +14,7 @@
 #define MAX_NAME_LENGTH 100
 #define MAX_SUBURB_LENGTH 50
 #define MAX_PATH_LENGTH 50
+#define MAX_SERVICES 50
 
  /**
   * Structure representing a bus stop in the transport network
@@ -40,6 +41,18 @@ typedef struct Connection
 } Connection;
 
 /**
+ * Structure representing an essential service
+ */
+typedef struct
+{
+    int service_id;
+    char name[MAX_NAME_LENGTH];
+    int stop_id;
+    char service_type[50];
+    double importance_weight;
+} EssentialService;
+
+/**
  * Adjacency list representation of transport network
  */
 typedef struct
@@ -48,6 +61,8 @@ typedef struct
     Connection** adjacency_list;
     int num_stops;
     int capacity;
+    EssentialService* services;
+    int num_services;
 } TransportGraph;
 
 /**
@@ -62,6 +77,17 @@ typedef struct
     bool path_found;
 } PathResult;
 
+/**
+ * Structure for accessibility analysis results
+ */
+typedef struct
+{
+    int stop_id;
+    double accessibility_score;
+    int avg_transfers_to_services;
+    int unreachable_services;
+} AccessibilityResult;
+
 // Core graph operations
 TransportGraph* create_transport_graph(int initial_capacity);
 int add_bus_stop(TransportGraph* graph, int stop_id, const char* name, double lat, double lon, const char* suburb);
@@ -72,6 +98,15 @@ void free_transport_graph(TransportGraph* graph);
 PathResult* find_shortest_path(TransportGraph* graph, int start_stop, int end_stop);
 void free_path_result(PathResult* result);
 void print_path(TransportGraph* graph, PathResult* result);
+
+// Accessibility analysis
+AccessibilityResult* calculate_accessibility_score(TransportGraph* graph, int stop_id);
+void analyse_network_accessibility(TransportGraph* graph);
+int* find_critical_stops(TransportGraph* graph, int* num_critical);
+
+// Services
+int add_essential_service(TransportGraph* graph, int service_id, const char* name, int stop_id, const char* service_type, double weight);
+int load_services_from_file(TransportGraph* graph, const char* filename);
 
 // Utility functions
 int find_stop_index(TransportGraph* graph, int stop_id);
